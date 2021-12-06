8c4c46861ed5da91f98fa71965c14848ca87ffad
Ubuntu <ubuntu@ip-172-31-14-70.us-east-2.compute.internal>
2021-12-06 08:56:30 +0000

    App.js modified to include my name

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+	<p><a href= "https://www.linkedin.com/in/gustave-am%C3%A8-gbossou/">
+		Ame Gbossou</a>
+	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
